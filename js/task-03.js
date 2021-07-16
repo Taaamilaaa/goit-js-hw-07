56c3357903e5f320fe2776acc1f06ceae7a0947d
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-07-16 19:09:35 +0300

    Добавляет 3-е задание

--- js/task-03.js
+++ js/task-03.js
@@ -13,50 +13,80 @@ const images = [
   },
 ];
 
-const galleryEl = document.querySelector("#gallery");
+const galleryContainerEl = document.querySelector("#gallery");
+galleryContainerEl.style.display = "flex";
+galleryContainerEl.style.listStyle = "none";
+galleryContainerEl.style.margin = "-15px";
 
-const elements = images.map((item) => {
-  const { url, alt } = item;
-  const el = document.createElement("li");
-  const img = document.createElement("img");
-    
-  img.src = url;
-  img.alt = alt;
-  img.style.width = "300px";
-  img.style.listStyleType = "none";
-  img.style.display = "flexWrap";
-  img.style.margin = "30px";
-  el.appendChild(img);
-  galleryEl.insertAdjacentElement("afterbegin", el);
-  return galleryEl;
-});
+const createElementOfGallery = (list) => {
+  const { url, alt } = list;
+  return `
+  <li>
+ <img src = ${url}
+ alt = ${alt}
+ width = '100%'
+ height = '100%'
+ </li >
+ <style>
+ li {margin: 15px;
+ border: 5px solid lightgrey;}
+ </style>
+  `
+};
 
-console.log(elements);
+const listOfImages = images
+  .map(createElementOfGallery)
+  .join(' ');
+  console.log(listOfImages);
+
+galleryContainerEl.insertAdjacentHTML('afterbegin', listOfImages)
+
+
 
 // const createGalleryList = (list) => {
-//   let el;
-//   let img;
-//   const arrItems = [];
-
-//   for (let item of list) {
-//     const { url, alt } = item;
-//     el = document.createElement("li");
-//     img = document.createElement("img");
-//     img.src = url;
-//     img.alt = alt;
-//       img.style.width = '300px';
-//       img.style.listStyleType = 'none';
-//       img.style.display = 'flex';
-//       img.style.justifyContent = 'spaceBetween'
-
-//     el.append(img);
-
-//     arrItems.push(el);
-//   }
-
-//   return arrItems;
+//   list.forEach((item) => {
+//     galleryContainerEl.insertAdjacentHTML(
+//       "afterbegin",
+//       `
+// <li>
+// <img src = ${images[list.indexOf(item)].url}
+// alt = ${images[list.indexOf(item)].alt}
+// width = '100%'
+// height = '100%'
+// </li >
+// <style>
+// li {margin: 15px;
+// border: 5px solid lightgrey;}
+// </style>
+// `
+//     );
+//   });
+//   return list;
 // };
-// const galleryListEl = createGalleryList(images);
-// console.log(galleryListEl);
 
-// galleryEl.append(...galleryListEl);
+// console.log(createGalleryList(images));
+
+
+
+// const createGalleryList = (list) => {
+//   return list.map(item => {
+//     const { url, alt } = item;
+//     const itemEl = document.createElement("li");
+//     itemEl.style.margin = '15px';
+//     itemEl.style.width = '400px';
+
+//     const imgEl = document.createElement("img");
+//     imgEl.src = url;
+//     imgEl.alt = alt;
+//     imgEl.style.height = '100%';
+//     imgEl.style.width = '100%';
+
+//     itemEl.append(imgEl);
+//     return itemEl;
+//   }
+//   );
+// }
+// console.log(createGalleryList(images));
+// const galleryList = createGalleryList(images);
+
+// galleryContainerEl.append(...galleryList);
